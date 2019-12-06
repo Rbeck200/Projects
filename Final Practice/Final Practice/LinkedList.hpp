041da2558ce96845c5f044a21d88b339a5434cd6
Rbeck200 <54686826+Rbeck200@users.noreply.github.com>
2019-12-05 23:14:56 -0800

    FInals quetions
    only questions 1 and 2. not to sure if 2 works properly cause of vector type

---
+++ Final Practice/Final Practice/LinkedList.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <vector>
+#include "ListNode.hpp"
+
+template<typename DATA_TYPE>
+class LinkedList {
+public:
+
+	ListNode* _front = nullptr;
+
+	int deleteNode(ListNode* front, int value) {
+		int counter = 0;
+
+		if (front != nullptr && front->value == value) {
+			ListNode* new_front = front->next;
+			delete front;
+			front = new_front;
+			return counter;
+		}
+
+		ListNode* current = front;
+		ListNode* before = nullptr;
+		while (current != nullptr && current->value != value) {
+			before = current;
+			current = current->next;
+			counter++;
+		}
+
+		if (current != nullptr) {
+			before->next = current->next;
+			delete current;
+			return counter;
+		}
+		else {
+			return -1;
+		}
+	}
+
+	vector<int> toArray(ListNode *front) {
+		vector<int> result{};
+		while (front != nullptr) {
+			result.push_back(front->value);
+			ListNode* new_front = front->next;
+			front = new_front;
+			delete new_front;
+		}
+		return result;
+	}
+
+
+
+	
+
+};
